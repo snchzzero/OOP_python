50ac0761ca8bc032e7c86dcc8ec468c3bb6fa5ec
snchzzero <aaevstafev@gmail.com>
2022-09-13 00:06:57 +0300

    Ex 2.1.9(2)

--- 2. Режимы доступа public, private, protected. Сеттеры и геттеры/Ex 2.1.9.py
+++ 2. Режимы доступа public, private, protected. Сеттеры и геттеры/Ex 2.1.9.py
@@ -5,41 +5,54 @@ class LinkedList:
         self.tail = tail
 
     def add_obj(self, obj):
-        print(f'В список добавлен элемент {obj}')
+        #print(f'В список добавлен элемент {obj}')
         if self.head_t == 0:
             self.head = obj
             self.tail = obj
             self.head_t = self.head_t + 1
             obj.set_prev = None
             #
-            print(f'Перед объектом {obj} стоит объект {obj.get_prev()}')
-            print(f'После объекта {obj} стоит объект {obj.get_next()}')
-            print(f'Всего объектов в списке {self.head_t}')
+            #print(f'Перед объектом {obj} стоит объект {obj.get_prev()}')
+            #print(f'После объекта {obj} стоит объект {obj.get_next()}')
+            #print(f'Всего объектов в списке {self.head_t}')
         else:
             self.tail.set_next(obj)
             obj.set_prev(self.tail)
             self.tail = obj
             self.head_t = self.head_t + 1
-            print(f'Перед объектом {obj} стоит объект {obj.get_prev()}')
-            print(f'После объекта {obj} стоит объект {obj.get_next()}')
-            print(f'Всего объектов в списке {self.head_t}')
+            #print(f'Перед объектом {obj} стоит объект {obj.get_prev()}')
+            #print(f'После объекта {obj} стоит объект {obj.get_next()}')
+            #print(f'Всего объектов в списке {self.head_t}')
 
     def remove_obj(self):
-        if self.head_t > 0:
-            print(f'В списке удален последний элемент {self.tail}')
+        if self.head_t > 1:
+            #print(f'В списке удален последний элемент {self.tail}')
             self.tail.get_prev().set_next(None)
             self.tail = self.tail.get_prev()
             self.head_t = self.head_t - 1
-            print(f'Теперь последний объект {self.tail}')
-            print(f'Перед объектом {self.tail} стоит объект {self.tail.get_prev()}')
-            print(f'После объекта {self.tail} стоит объект {self.tail.get_next()}')
-            print(f'Всего объектов в списке {self.head_t}')
-
+            #print(f'Теперь последний объект {self.tail}')
+            #print(f'Перед объектом {self.tail} стоит объект {self.tail.get_prev()}')
+            #print(f'После объекта {self.tail} стоит объект {self.tail.get_next()}')
+            #print(f'Всего объектов в списке {self.head_t}')
+        elif self.head_t == 1:
+            #print(f'В списке удален последний элемент {self.tail}')
+            self.tail = None
+            self.head_t = None
 
 
     def get_data(self):
-        print(f'Возращение __data всех объектов списка {LinkedList.list}')
-        print([i.get_data for i in LinkedList.list])
+        #print(f'Возращение __data всех объектов списка')
+        start = self.head
+        l1 = list()
+        #print(start.get_data())
+        while start.get_next() != None and self.head != None and self.tail != None:
+            l1.append(start.get_data())
+            start = start.get_next()
+        if start.get_next() == None and self.head != None and self.tail != None:
+            l1.append(start.get_data())
+        #print(l1)
+        return l1
+
 
 class ObjList:
     def __init__(self, data=None, next=None, prev=None):
@@ -65,10 +78,68 @@ class ObjList:
     def get_data(self):
         return self.__data
 
-lst = LinkedList()
-print(f'Создан объект LinkedList {lst}')
-lst.add_obj(ObjList("данные 1"))
-lst.add_obj(ObjList("данные 2"))
-lst.add_obj(ObjList("данные 3"))
-lst.remove_obj()
+# lst = LinkedList()
+# print(f'Создан объект LinkedList {lst}')
+# lst.add_obj(ObjList("данные 1"))
+# lst.add_obj(ObjList("данные 2"))
+# lst.add_obj(ObjList("данные 3"))
+# lst.get_data()
+# lst.remove_obj()
+# lst.get_data()
 
+ls = LinkedList()
+ls.add_obj(ObjList("данные 1"))
+ls.add_obj(ObjList("данные 2"))
+ls.add_obj(ObjList("данные 3"))
+ls.add_obj(ObjList("данные 34"))
+assert ls.get_data() == ['данные 1', 'данные 2', 'данные 3', 'данные 34'], "метод get_data вернул неверные данные"
+
+ls_one = LinkedList()
+ls_one.add_obj(ObjList(1))
+assert ls_one.get_data() == [1], "метод get_data вернул неверные данные"
+
+h = ls_one.head
+n = 0
+while h:
+    n += 1
+    h = h.get_next()
+
+assert n == 1, "неверное число объектов в списке: возможно некорректно работает метод add_obj"
+ls_one.remove_obj()
+assert ls_one.get_data() == [], "метод get_data вернул неверные данные для пустого списка, возможно, неверно работает метод remove_obj"
+
+ls2 = LinkedList()
+assert ls.head != ls2.head, "атрибут head должен принадлежать объекту класса LinkedList, а не самому классу"
+assert ls.tail != ls2.tail, "атрибут tail должен принадлежать объекту класса LinkedList, а не самому классу"
+
+h = ls.head
+n = 0
+while h:
+    n += 1
+    h = h.get_next()
+
+assert n == 4, "неверное число объектов в списке: возможно некорректно работает метод add_obj"
+
+h = ls.head
+n = 0
+while h:
+    h = h._ObjList__next
+    n += 1
+
+assert n == 4, "неверное число объектов в списке: возможно некорректные значения в атрибутах __next"
+
+h = ls.tail
+n = 0
+while h:
+    n += 1
+    h = h.get_prev()
+
+assert n == 4, "неверное число объектов в списке: возможно некорректно работает метод add_obj"
+
+h = ls.tail
+n = 0
+while h:
+    h = h._ObjList__prev
+    n += 1
+
+assert n == 4, "неверное число объектов в списке: возможно некорректные значения в атрибутах __prev"
