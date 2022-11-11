6abee9b061c2a50304f96dc6284bb6e4b776254e
snchzzero <aaevstafev@gmail.com>
2022-11-11 19:13:46 +0300

    Ex 3.1.8

---
+++ 3.1 Методы __setattr__, __getattribute__, __getattr__ и __delattr_/Ex 3.1.8.py
@@ -0,0 +1,32 @@
+class SmartPhone:
+    def __init__(self, model):
+        self.model = model
+        self.apps = []
+
+    def add_app(self, app):
+        if app.__class__ not in [_.__class__ for _ in self.apps]:
+            self.apps.append(app)
+
+    def remove_app(self, app):
+        self.apps.remove(app)
+
+class AppVK:
+    def __init__(self):
+        self.name = "ВКонтакте"
+
+class AppYouTube:
+    def __init__(self, memory_max):
+        self.name = "YouTube"
+        self.memory_max = memory_max
+
+class AppPhone:
+    def __init__(self, phone_list):
+        self.name = "Phone"
+        self.phone_list = phone_list
+
+sm = SmartPhone("Honor 1.0")
+sm.add_app(AppVK())
+sm.add_app(AppVK())  # второй раз добавляться не должно
+sm.add_app(AppYouTube(2048))
+for a in sm.apps:
+    print(a.name)
