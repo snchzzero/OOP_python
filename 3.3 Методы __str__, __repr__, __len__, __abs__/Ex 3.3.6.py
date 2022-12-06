8c9b388b24081e477874e08ee382fa2544737c9c
snchzzero <aaevstafev@gmail.com>
2022-12-06 22:47:09 +0300

    Ex 3.3.6(2)

--- 3.3 Методы __str__, __repr__, __len__, __abs__/Ex 3.3.6.py
+++ 3.3 Методы __str__, __repr__, __len__, __abs__/Ex 3.3.6.py
@@ -1,14 +1,15 @@
 class LinkedList:
-    def __init__(self, head=None, tail=None):
+    def __init__(self, head=None, tail=None, l1=[]):
         self.head = head
         self.tail = tail
+        self.l1 = l1
 
-    def add_obj(self, obj, l1=[]):
+    def add_obj(self, obj):
         if self.head == None:
             self.head, self.tail = obj, obj
         else:
-            self.tail.__next = obj
-            obj.__prev = self.tail
+            self.tail.next = obj
+            obj.prev = self.tail
             self.tail = obj
         self.l1.append(obj)
 
@@ -16,22 +17,33 @@ class LinkedList:
         total = 0
         obj = self.head
         while total < indx:
-            obj = obj.__next
+            obj = obj.next
             total += 1
         if obj != self.tail and obj != self.head:
-            obj.__prev.__next = obj.__next
-            obj.__next.__prev = obj.__prev
-        elif obj == self.tail:
-            obj.__prev.__next = None
-            self.tail = obj
-        elif obj == self.head:
-            obj.__next.__prev = None
-            self.head = obj
-        self.l1.remove(indx)
+            obj.prev.next = obj.next
+            obj.next.prev = obj.prev
+        elif obj == self.tail and len(self.l1) > 1:
+            obj.prev.next = None
+            self.tail = obj.prev
+        elif obj == self.head and len(self.l1) > 1:
+            obj.next.prev = None
+            self.head = obj.next
+        elif len(self.l1) == 1:
+            self.head = None
+            self.tail = None
+        self.l1.pop(indx)
 
     def __len__(self):
         return len(self.l1)
 
+    def __call__(self, *args, **kwargs):
+        #print(self.linked_lst(args[0]))
+        #print(self.l1[args[0]].data)
+        return self.linked_lst(args[0])
+
+    def linked_lst(self, index):
+        #print(self.l1[index].get_data())
+        return self.l1[index].data
 
 
 class ObjList:
@@ -39,6 +51,26 @@ class ObjList:
         self.__data = data
         self.__prev = prev
         self.__next = next
+
+    @property
+    def prev(self):
+        return self.__prev
+
+    @prev.setter
+    def prev(self, obj):
+        self.__prev = obj
+
+    @property
+    def next(self):
+        return self.__next
+
+    @next.setter
+    def next(self, obj):
+        self.__next = obj
+
+    @property
+    def data(self):
+        return self.__data
 
 # linked_lst = LinkedList()
 # linked_lst.add_obj(ObjList("Sergey"))
@@ -53,6 +85,7 @@ ln = LinkedList()
 ln.add_obj(ObjList("Сергей"))
 ln.add_obj(ObjList("Балакирев"))
 ln.add_obj(ObjList("Python ООП"))
+#ln.remove_obj(1)
 ln.remove_obj(2)
 assert len(ln) == 2, "функция len вернула неверное число объектов в списке, возможно, неверно работает метод remove_obj()"
 ln.add_obj(ObjList("Python"))
