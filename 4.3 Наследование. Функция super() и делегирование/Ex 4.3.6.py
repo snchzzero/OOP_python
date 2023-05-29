e054fcb0a2a81fabfb1daa7fc65a3a1a02d4f3de
snchzzero <aaevstafev@gmail.com>
2023-05-29 16:55:02 +0300

    ex 4.3.6, 4.3.7 done

---
+++ 4.3 Наследование. Функция super() и делегирование/Ex 4.3.6.py
@@ -0,0 +1,58 @@
+class SellItem:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class House(SellItem):
+    def __init__(self, name, price, material, square):
+        super().__init__(name, price)
+        self.material = material
+        self.square = square
+
+
+class Flat(SellItem):
+    def __init__(self, name, price, size, rooms):
+        super().__init__(name, price)
+        self.size = size
+        self.rooms = rooms
+
+
+class Land(SellItem):
+    def __init__(self, name, price, square):
+        super().__init__(name, price)
+        self.square = square
+
+
+class Agency:
+    def __init__(self, name):
+        self.name = name
+        self.list = []
+
+    def add_object(self, obj):
+        self.list.append(obj)
+
+    def remove_object(self, obj):
+        for elem in self.list:
+            if elem == obj:
+                self.list.remove(obj)
+
+    def get_objects(self):
+        return self.list
+
+
+ag = Agency("Рога и копыта")
+ag.add_object(Flat("квартира, 3к", 10000000, 121.5, 3))
+ag.add_object(Flat("квартира, 2к", 8000000, 74.5, 2))
+ag.add_object(Flat("квартира, 1к", 4000000, 54, 1))
+ag.add_object(House("дом, крипичный", price=35000000, material="кирпич", square=186.5))
+new_h = House("кошелек", price=1, material="картон", square=3)
+ag.add_object(new_h)
+ag.add_object(Land("участок под застройку", 3000000, 6.74))
+for obj in ag.get_objects():
+    print(obj.name)
+
+lst_houses = [x for x in ag.get_objects() if isinstance(x, House)] # выделение списка домов
+print(lst_houses)
+ag.remove_object(new_h)
+print(ag.get_objects())
---
+++ 4.3 Наследование. Функция super() и делегирование/Ex 4.3.7.py
@@ -0,0 +1,32 @@
+class Router:
+    app = {}
+
+    @classmethod
+    def get(cls, path):
+        return cls.app.get(path)
+
+    @classmethod
+    def add_callback(cls, path, func):
+        cls.app[path] = func
+
+
+class Callback:
+    def __init__(self, path, route_cls):
+        self.path = path
+        self.route_cls = route_cls
+
+    def __call__(self, func):
+        self.route_cls.add_callback(self.path, func)
+
+
+@Callback('/about', Router)
+def about():
+    return '<h1>About</h1>'
+print(Router.app)
+
+route = Router.get('/about')
+ret = route()
+assert ret == '<h1>About</h1>', "декорированная функция вернула неверные данные"
+
+route = Router.get('/')
+assert route is None, "Класс Router, при вызове метода get, вернул неверные данные"
