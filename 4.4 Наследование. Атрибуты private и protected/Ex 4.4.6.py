3f7fed80e5c8224234b60eba48cf145cc716685f
snchzzero <aaevstafev@gmail.com>
2023-05-31 18:08:00 +0300

    ex 4.4.6, 4.4.7 done

---
+++ 4.4 Наследование. Атрибуты private и protected/Ex 4.4.6.py
@@ -0,0 +1,38 @@
+class Animal:
+    def __init__(self, name, kind, old):
+        self.__name = name
+        self.__kind = kind
+        self.__old = old
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def kind(self):
+        return self.__kind
+
+    @kind.setter
+    def kind(self, value):
+        self.__kind = value
+
+    @property
+    def old(self):
+        return self.__old
+
+    @old.setter
+    def old(self, value):
+        self.__old = value
+
+
+animals = [Animal('Васька', 'дворовый кот', 5), Animal('Рекс', 'немецкая овчарка', 8), Animal('Кеша', 'попугай', 3) ]
+animals[0].name = 'Петька'
+print(animals[0].name)
+animals[1].kind = 'такса'
+print(animals[1].kind)
+animals[2].old = 77
+print(animals[2].old)
