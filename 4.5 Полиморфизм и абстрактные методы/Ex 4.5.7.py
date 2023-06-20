b50b5c5f78665b94ab6b5d09b9990706b179417d
snchzzero <aaevstafev@gmail.com>
2023-06-20 13:36:17 +0300

    ex 4.5.7 repaired

--- 4.5 Полиморфизм и абстрактные методы/Ex 4.5.7.py
+++ 4.5 Полиморфизм и абстрактные методы/Ex 4.5.7.py
@@ -1,69 +1,101 @@
 from abc import ABC, abstractmethod
 
 
-class CountryInterface(ABC):
+class StackInterface(ABC):
 
-    @property
     @abstractmethod
-    def name(self):
-        pass
-
-    @property
-    @abstractmethod
-    def population(self):
-        pass
-
-    @property
-    @abstractmethod
-    def square(self):
+    def push_back(self, obj):
         pass
 
     @abstractmethod
-    def get_info(self):
+    def pop_back(self):
         pass
 
 
-class Country(CountryInterface):
+class Stack(StackInterface):
+    def __init__(self, top=None):
+        self._top = top
 
-    def __init__(self, name, population, square):
-        self._name = name
-        self._population = population
-        self._square = square
+    def push_back(self, obj):
+        if self._top is None:
+            self._top = obj
+        else:
+            obj_iter = self._top
+            while obj_iter._next is not None:
+                obj_iter = obj_iter._next
+            obj_iter._next = obj
 
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        self._name = value
-
-    @property
-    def population(self):
-        return self._population
-
-    @population.setter
-    def population(self, value):
-        self._population = value
-
-    @property
-    def square(self):
-        return self._square
-
-    @square.setter
-    def square(self, value):
-        self._square = value
-
-    def get_info(self):
-        return f'{self._name}: {self._square}, {self._population}'
+    def pop_back(self):
+        obj_iter = self._top
+        obj_prev = None
+        if obj_iter:
+            while obj_iter._next is not None:
+                obj_prev = obj_iter
+                obj_iter = obj_iter._next
+            if obj_prev:
+                obj_prev._next = None
+            else:
+                self._top = None
+        return obj_iter
 
 
-country = Country("Россия", 140000000, 324005489.55)
-name = country.name
-pop = country.population
-country.population = 150000000
-country.square = 354005483.0
-print(country.get_info())  # Россия: 354005483.0, 150000000
+class StackObj:
+    def __init__(self, data, next=None):
+        self._data = data
+        self._next = next
+
+
+# st = Stack()
+# del_obj = st.pop_back()
+#
+# st.push_back(StackObj("obj 1"))
+# obj = StackObj("obj 2")
+# st.push_back(obj)
+# st.push_back(StackObj("obj 3"))
+# st.push_back(StackObj("obj 44"))
+# st.push_back(StackObj("obj 55"))
+# del_obj = st.pop_back()
+# st.push_back(StackObj("obj 6"))
+# # del_obj - ссылка на удаленный объект (если объектов не было, то del_obj = None)
+
+assert issubclass(Stack, StackInterface), "класс Stack должен наследоваться от класса StackInterface"
+
+try:
+    a = StackInterface()
+    a.pop_back()
+except TypeError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение TypeError при вызове абстрактного метода класса StackInterface"
+
+
+st = Stack()
+assert st._top is None, "атрибут _top для пустого стека должен быть равен None"
+
+obj_top = StackObj("obj")
+st.push_back(obj_top)
+
+assert st._top == obj_top, "неверное значение атрибута _top"
+
+obj = StackObj("obj")
+st.push_back(obj)
+
+n = 0
+h = st._top
+while h:
+    assert h._data == "obj", "неверные данные в объектах стека"
+    h = h._next
+    n += 1
+
+assert n == 2, "неверное число объектов в стеке (или структура стека нарушена)"
+
+del_obj = st.pop_back()
+assert del_obj == obj, "метод pop_back возвратил неверный объект"
+
+del_obj = st.pop_back()
+assert del_obj == obj_top, "метод pop_back возвратил неверный объект"
+
+assert st._top is None, "неверное значение атрибута _top"
 
 
 
