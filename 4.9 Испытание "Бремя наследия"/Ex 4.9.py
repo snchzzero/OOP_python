a3256d4cd089a672022a462ac0f6d109549f783f
snchzzero <aaevstafev@gmail.com>
2023-09-06 19:05:24 +0300

    task checked after test

--- 4.9 Испытание "Бремя наследия"/Ex 4.9.py
+++ 4.9 Испытание "Бремя наследия"/Ex 4.9.py
@@ -1,6 +1,5 @@
 import math
 
-
 class Vertex:
     def __init__(self):
         self._links = []
@@ -99,8 +98,7 @@ class LinkedGraph:
             if v >= 0:  # выбрана очередная вершина
                 #S.add(v)  # добавляем новую вершину в рассмотрение
                 S[v] = self._vertex[v]
-
-        print(T)  # [0, 1, 1, 4, 5, 7, 6]
+        #print(T)  # [0, 1, 1, 4, 5, 7, 6]
 
         # формирование оптимального маршрута:
         start = self.get_index(start_v)
@@ -110,13 +108,8 @@ class LinkedGraph:
             end = M[P[-1]]
             P.append(end)
         P.reverse()
-
-        print(P)  # [5, 6, 1, 0] -> [0, 1, 6, 5]
+        #print(P)  # [5, 6, 1, 0] -> [0, 1, 6, 5]
         return P
-
-
-
-
 
     def find_path(self, start_v, stop_v):
         shotest_way_index = self.matrix(start_v, stop_v)
@@ -135,13 +128,6 @@ class LinkedGraph:
                         break
         result = (stations, links)
         return result
-
-
-
-
-
-
-
 
 
 class Station(Vertex):
@@ -188,28 +174,77 @@ class LinkMetro(Link):
 # path = map_graph.find_path(v1, v6)
 
 
-map_metro = LinkedGraph()
-v1 = Station("Сретенский бульвар")
-v2 = Station("Тургеневская")
-v3 = Station("Чистые пруды")
-v4 = Station("Лубянка")
-v5 = Station("Кузнецкий мост")
-v6 = Station("Китай-город 1")
-v7 = Station("Китай-город 2")
+# map_metro = LinkedGraph()
+# v1 = Station("Сретенский бульвар")
+# v2 = Station("Тургеневская")
+# v3 = Station("Чистые пруды")
+# v4 = Station("Лубянка")
+# v5 = Station("Кузнецкий мост")
+# v6 = Station("Китай-город 1")
+# v7 = Station("Китай-город 2")
+#
+# map_metro.add_link(LinkMetro(v1, v2, 1))
+# map_metro.add_link(LinkMetro(v2, v3, 1))
+# map_metro.add_link(LinkMetro(v1, v3, 1))
+#
+# map_metro.add_link(LinkMetro(v4, v5, 1))
+# map_metro.add_link(LinkMetro(v6, v7, 1))
+#
+# map_metro.add_link(LinkMetro(v2, v7, 5))
+# map_metro.add_link(LinkMetro(v3, v4, 3))
+# map_metro.add_link(LinkMetro(v5, v6, 3))
+#
+# print(len(map_metro._links))  # 8 связей
+# print(len(map_metro._vertex))  # 7 вершин
+# path = map_metro.find_path(v1, v6)  # от сретенского бульвара до китай-город 1
+# print(path[0])    # [Сретенский бульвар, Тургеневская, Китай-город 2, Китай-город 1]
+# print(sum([x.dist for x in path[1]]))  # 7
 
-map_metro.add_link(LinkMetro(v1, v2, 1))
-map_metro.add_link(LinkMetro(v2, v3, 1))
-map_metro.add_link(LinkMetro(v1, v3, 1))
 
-map_metro.add_link(LinkMetro(v4, v5, 1))
-map_metro.add_link(LinkMetro(v6, v7, 1))
+### TESTS ###
+map2 = LinkedGraph()
+v1 = Vertex()
+v2 = Vertex()
+v3 = Vertex()
+v4 = Vertex()
+v5 = Vertex()
 
-map_metro.add_link(LinkMetro(v2, v7, 5))
-map_metro.add_link(LinkMetro(v3, v4, 3))
-map_metro.add_link(LinkMetro(v5, v6, 3))
+map2.add_link(Link(v1, v2))
+map2.add_link(Link(v2, v3))
+map2.add_link(Link(v2, v4))
+map2.add_link(Link(v3, v4))
+map2.add_link(Link(v4, v5))
 
-print(len(map_metro._links))  # 8 связей
-print(len(map_metro._vertex))  # 7 вершин
-path = map_metro.find_path(v1, v6)  # от сретенского бульвара до китай-город 1
-print(path[0])    # [Сретенский бульвар, Тургеневская, Китай-город 2, Китай-город 1]
-print(sum([x.dist for x in path[1]]))  # 7
+assert len(map2._links) == 5, "неверное число связей в списке _links класса LinkedGraph"
+assert len(map2._vertex) == 5, "неверное число вершин в списке _vertex класса LinkedGraph"
+
+map2.add_link(Link(v2, v1))
+assert len(map2._links) == 5, "метод add_link() добавил связь Link(v2, v1), хотя уже имеется связь Link(v1, v2)"
+
+path = map2.find_path(v1, v5)
+s = sum([x.dist for x in path[1]])
+assert s == 3, "неверная суммарная длина маршрута, возможно, некорректно работает объект-свойство dist"
+
+assert issubclass(Station, Vertex) and issubclass(LinkMetro, Link), "класс Station должен наследоваться от класса Vertex, а класс LinkMetro от класса Link"
+
+map2 = LinkedGraph()
+v1 = Station("1")
+v2 = Station("2")
+v3 = Station("3")
+v4 = Station("4")
+v5 = Station("5")
+
+map2.add_link(LinkMetro(v1, v2, 1))
+map2.add_link(LinkMetro(v2, v3, 2))
+map2.add_link(LinkMetro(v2, v4, 7))
+map2.add_link(LinkMetro(v3, v4, 3))
+map2.add_link(LinkMetro(v4, v5, 1))
+
+assert len(map2._links) == 5, "неверное число связей в списке _links класса LinkedGraph"
+assert len(map2._vertex) == 5, "неверное число вершин в списке _vertex класса LinkedGraph"
+
+path = map2.find_path(v1, v5)
+
+assert str(path[0]) == '[1, 2, 3, 4, 5]', path[0]
+s = sum([x.dist for x in path[1]])
+assert s == 7, "неверная суммарная длина маршрута для карты метро"
