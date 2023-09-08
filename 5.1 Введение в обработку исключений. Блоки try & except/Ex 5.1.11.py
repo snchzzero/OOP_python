23e8fa9b2b60aa726fd251e7145762f78fc78045
snchzzero <aaevstafev@gmail.com>
2023-09-08 08:23:51 +0300

    Ex 5.1.11 - done

--- 5.1 Введение в обработку исключений. Блоки try & except/Ex 5.1.11.py
+++ 5.1 Введение в обработку исключений. Блоки try & except/Ex 5.1.11.py
@@ -4,8 +4,10 @@ class FloatValidator:
         self.max_value = max_value
 
     def __call__(self, *args, **kwargs):
-        if not isinstance(args[0], float) or not self.min_value <= args[0] < self.max_value:
-            raise ValueError('значение не прошло валидацию')
+        if isinstance(args[0], float):
+            if self.min_value < args[0] <= self.max_value:
+                return
+        raise ValueError('значение не прошло валидацию')
 
 
 class IntegerValidator:
@@ -14,26 +16,28 @@ class IntegerValidator:
         self.max_value = max_value
 
     def __call__(self, *args, **kwargs):
-        if not isinstance(args[0], int) or not self.min_value < args[0] <= self.max_value or isinstance(args[0], bool):
-            raise ValueError('значение не прошло валидацию')
+        if type(args[0]) == int:
+            if self.min_value < args[0] <= self.max_value:
+                return
+        raise ValueError('значение не прошло валидацию')
 def is_valid(lst, validators):
     new_lst = []
     for value in lst:
         try:
             validators[0](value)
-        except ValueError:
+        except Exception:
             try:
                 validators[1](value)
-            except ValueError:
+            except Exception:
                 continue
         new_lst.append(value)
     return new_lst
 
 
 
-fv = FloatValidator(0, 10.5)
+fv = FloatValidator(-1, 12.5)
 iv = IntegerValidator(-10, 20)
-lst_out = is_valid([1, 4.5, -10.5, 100, True, 'abc', (1, 2)], validators=[fv, iv])   # [1, 4.5]
+lst_out = is_valid([False, 0, 1, 4.5, -10.5, 100, True, 'abc', (1, 2)], validators=[fv, iv])   # [1, 4.5]
 
 print(lst_out)
 
