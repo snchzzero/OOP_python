c710f6351aa936f196c33d17f6df61734d1a8504
snchzzero <aaevstafev@gmail.com>
2023-09-12 17:47:41 +0300

    Ex 5.2.9 - done

--- 5.2 Обработка исключений. Блоки finally и else/Ex 5.2.9.py
+++ 5.2 Обработка исключений. Блоки finally и else/Ex 5.2.9.py
@@ -1,7 +1,7 @@
 class Rect:
     def __init__(self, x, y, width, height):
         if not all([isinstance(param, (int, float))for param in [x, y, width, height]]) \
-                or not width >= 0 or not height >= 0:
+                or not width > 0 or not height > 0:
             raise ValueError('некорректные координаты и параметры прямоугольника')
         self._x = x
         self._y = y
@@ -9,16 +9,17 @@ class Rect:
         self._height = height
 
     def is_collision(self, rect):
-        point_x_self = [i for i in range(self._x, self._x + self._width + 1)]
-        point_y_self = [i for i in range(self._y, self._y + self._height + 1)]
-        point_x_rect = [i for i in range(rect._x, rect._x + rect._width + 1)]
-        point_y_rect = [i for i in range(rect._y, rect._y + rect._height + 1)]
-        if not all([pxs not in point_x_rect for pxs in point_x_self]) or \
-                not all([pys not in point_y_rect for pys in point_y_self]):
-                raise TypeError('прямоугольники пересекаются')
-        else:
-            return True
 
+        if any(
+                [
+                    self._y > (rect._y + rect._height),
+                    (self._y + self._height) < rect._y,
+                    self._x > (rect._x + rect._width),
+                    (self._x + self._width) < rect._x
+                ]
+        ):
+            return True
+        raise TypeError('прямоугольники пересекаются')
 
 
 lst_rect = []
@@ -29,23 +30,68 @@ for param in [
     [0, 8, 8, 1]
 ]:
     lst_rect.append(Rect(param[0], param[1], param[2], param[3]))
-
+#
 lst_not_collision = []
+lst_failed = []
 for rect in lst_rect:
+    if rect in lst_failed:
+        continue
     try:
         for x in lst_rect:
             if rect != x:
                 try:
                     rect.is_collision(x)
                 except Exception:
+                    lst_failed.append(x)
                     raise TypeError('прямоугольники пересекаются')
+        lst_not_collision.append(rect)
     except Exception:
         continue
-    finally:
-        lst_not_collision.append(rect)
+#
+#
+# print(lst_not_collision)
+# print(lst_not_collision)
 
-print(lst_not_collision)
-print(lst_not_collision)
+
+
+
+r = Rect(1, 2, 10, 20)
+assert r._x == 1 and r._y == 2 and r._width == 10 and r._height == 20, "неверные значения атрибутов объекта класса Rect"
+
+r2 = Rect(1.0, 2, 10.5, 20)
+
+try:
+    r2 = Rect(0, 2, 0, 20)
+except ValueError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение ValueError при создании объекта Rect(0, 2, 0, 20)"
+
+
+assert len(lst_rect) == 4, "список lst_rect содержит не 4 элемента"
+assert len(lst_not_collision) == 1, "неверное число элементов в списке lst_not_collision"
+
+def not_collision(rect):
+    for x in lst_rect:
+        try:
+            if x != rect:
+                rect.is_collision(x)
+        except TypeError:
+            return False
+    return True
+
+f = list(filter(not_collision, lst_rect))
+assert lst_not_collision == f, "неверно выделены не пересекающиеся прямоугольники, возможно, некорректно работает метод is_collision"
+
+r = Rect(3, 2, 2, 5)
+rr = Rect(1, 4, 6, 2)
+
+try:
+    r.is_collision(rr)
+except TypeError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение TypeError при вызове метода is_collision() для прямоугольников Rect(3, 2, 2, 5) и Rect(1, 4, 6, 2)"
 
 
 
