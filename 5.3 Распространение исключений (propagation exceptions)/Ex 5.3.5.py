efa6d6c7d62d2c36aa043250fb2fe261b17902ca
snchzzero <aaevstafev@gmail.com>
2023-09-13 08:51:07 +0300

    Ex 5.3.5 - done

--- 5.3 Распространение исключений (propagation exceptions)/Ex 5.3.5.py
+++ 5.3 Распространение исключений (propagation exceptions)/Ex 5.3.5.py
@@ -1,20 +1,21 @@
 class ValidatorString:
-    def __init__(self, min_length, max_length, chars):
+    def __init__(self, min_length, max_length, chars=None):
         self.min_length = min_length
         self.max_length = max_length
         self.chars = chars
 
     def is_valid(self, string):
-        if not self.chars:
+        try:
             if type(string) == str and self.min_length <= len(string) <= self.max_length:
-                return True
+                if not self.chars:
+                    return string
+                if any([s in self.chars for s in string]):
+                    return string
             raise ValueError('недопустимая строка')
-        elif type(string) == str and self.min_length <= len(string) <= self.max_length:
-            lst = [s in self.chars for s in string]
-            if any(lst):
-                return True
-            raise ValueError('недопустимая строка')
-        raise ValueError('недопустимая строка')
+        except Exception:
+            return None
+
+
 
 class LoginForm:
     def __init__(self, login_validator, password_validator):
@@ -24,19 +25,34 @@ class LoginForm:
         self._password = None
 
     def form(self, request):
-        if 'login' not in request and 'password' not in request:
+        if 'login' not in request or 'password' not in request:
             raise TypeError('в запросе отсутствует логин или пароль')
-        if self.login_validator.is_valid(request.get('login')):
-            if self.password_validator.is_valid(request.get('password')):
-                self._login = request['login']
-                self._password = request['password']
+        for atr in ['_login', '_password']:
+            if atr == '_login':
+                value = self.login_validator.is_valid(request.get(atr[1:]))
+            elif atr == '_password':
+                value = self.password_validator.is_valid(request.get(atr[1:]))
+            setattr(self, atr, value)
+            # try:
+            #     value = self.login_validator.is_valid(request.get(atr[1:]))
+            #     setattr(self, atr, value)
+            # except Exception:
+            #     continue
+
+        # self._login = self.login_validator.is_valid(request.get('login'))
+        # self._passwor = self.password_validator.is_valid(request.get('password'))
 
 
 
-login_v = ValidatorString(4, 50, "")
-password_v = ValidatorString(10, 50, "!$#@%&?")
+
+
+
+login_v = ValidatorString(4, 50, )
+password_v = ValidatorString(10, 50, "$#@%&?")
 lg = LoginForm(login_v, password_v)
-login, password = input().split()
+#login, password = input().split()
+login, password = 'sergey', 'balakirev!'
+
 try:
     lg.form({'login': login, 'password': password})
 except (TypeError, ValueError) as e:
